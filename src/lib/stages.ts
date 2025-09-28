a858ea7765cf2519fea2b9862d54b65ff0ba4b9c
NicolasCDT03 <nico@fin.solar>
2025-09-28 13:46:55 -0600

    feat: improve investor deadlines editor

---
+++ src/lib/stages.ts
@@ -0,0 +1,13 @@
+export const STAGES = [
+  "Primera reunión",
+  "NDA",
+  "Entrega de información",
+  "Generación de propuesta",
+  "Presentación de propuesta",
+  "Ajustes técnicos",
+  "LOI",
+  "Revisión de contratos",
+  "Due Diligence",
+  "Cronograma de inversión",
+  "Firma",
+];
